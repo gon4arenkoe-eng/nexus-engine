eb0a5a1032911e40c0426a59aa4e52a49eab63e9
gon4arenkoe-eng <gon4arenko.e@gmail.com>
2026-07-21 22:16:42 +0300

    удаление перед заливкой

--- services/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Services package."""
-from .auth_service import AuthService, require_auth
-from .exchange_service import ExchangeService
-from .trading_service import TradingService
-
-__all__ = ["AuthService", "require_auth", "ExchangeService", "TradingService"]
--- strategies/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Trading strategies package."""
-from .base import BaseStrategy
-from .ema_cross import EmaCrossStrategy
-from .mean_reversion import MeanReversionStrategy
-
-__all__ = ["BaseStrategy", "EmaCrossStrategy", "MeanReversionStrategy"]
