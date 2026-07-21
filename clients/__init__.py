a206bd7f0971c88e01d23f9db44ec097ae93a106
gon4arenkoe-eng <gon4arenko.e@gmail.com>
2026-07-21 22:28:52 +0300

    удаление

--- clients/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Exchange clients package."""
-from .base import BaseExchangeClient
-from .bingx import BingXClient
-from .binance import BinanceClient
-from .bybit import BybitClient
-from .okx import OKXClient
-
-__all__ = ["BaseExchangeClient", "BingXClient", "BinanceClient", "BybitClient", "OKXClient"]
