2e01c360105b441994a0b20059d41cb0b3ac6a0a
gon4arenkoe-eng <gon4arenko.e@gmail.com>
2026-07-23 08:14:13 +0300

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,6 @@
 """Auth tests."""
 import pytest
-from app import create_app, db
+from app import create_app
 
 
 @pytest.fixture
@@ -8,11 +8,7 @@ def app():
     """Create test app."""
     app = create_app()
     app.config["TESTING"] = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
-    with app.app_context():
-        db.create_all()
-        yield app
-        db.drop_all()
+    return app
 
 
 @pytest.fixture
